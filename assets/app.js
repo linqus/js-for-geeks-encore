44cf5b4d98aa55bc4c330fe10f6f954dcad314b7
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-17 23:53:41 +0200

    move code to module, import jquery, bootstrap, remove js and script tags from template

--- assets/app.js
+++ assets/app.js
@@ -9,6 +9,19 @@
 import './styles/app.css';
 import get_message from './js/get_nice_message';
 
+import $ from 'jquery';
+import 'bootstrap';
+//// avoid globals
+// global.$ = $;
+
 console.log(get_message(5));
 // start the Stimulus application
 //import './bootstrap';
+
+$('.dropdown-toggle').dropdown();
+$('.custom-file-input').on('change', function(event) {
+    var inputFile = event.currentTarget;
+    $(inputFile).parent()
+        .find('.custom-file-label')
+        .html(inputFile.files[0].name);
+});
